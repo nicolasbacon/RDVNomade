26fe60d9adf4611e64823e5e6ee2e5a39b37f5d4
Vickorock <vickobernard35@gmail.com>
2020-07-06 16:59:54 +0200

    Maj victor avancée numéro 1

---
+++ src/Security/AdminChecker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AdminChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return 1;
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+        $this->checkPreAuth($user);
+    }
+}
